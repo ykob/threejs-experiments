cc1a8f159a62ac48eccce840088b5e4676559906
ykob <info@tplh.net>
2025-10-14 19:46:27 +0900

    fix debounce function.

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,6 +1,9 @@
-export const debounce = (fn: Function, delay: number) => {
-  let timer: number;
-  return (...args: any) => {
+export const debounce = <T extends (...args: unknown[]) => unknown>(
+  fn: T,
+  delay: number,
+) => {
+  let timer: ReturnType<typeof setTimeout>;
+  return (...args: Parameters<T>): void => {
     clearTimeout(timer);
     timer = setTimeout(() => {
       fn(...args);
